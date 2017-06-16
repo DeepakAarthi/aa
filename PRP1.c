d51be5909a41af3222e9675dbad8bfd14a0d4498
DeepakAarthi <aarthiselvamani3@gmail.com>
2017-06-16 14:58:50 +0530

    Add files via upload

---
+++ PRP1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,y,z;
+printf("\n number of coins ");
+scanf("%d",&x);
+printf("\n enter the grandchild");
+scanf("%d",&y);
+z=18*y;
+printf("\n total value of the coins each grandchild gets:%d",z);
+getch();
+}
